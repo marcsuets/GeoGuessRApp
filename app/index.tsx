a78861de7626e424f4779408af0b75d63824ebef
marcsuets <150144779+marcsuets@users.noreply.github.com>
2024-11-06 09:58:41 +0100

    Upload colors.js
     (color palette en variables)

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Hola</Text>
+      <Text>PDPDPDPDP</Text>
     </View>
   );
 }
---
+++ colors.js
@@ -0,0 +1,7 @@
+export default {
+    main: "#002642",
+    secondary: "#CCFF00",
+    black: "#111111",
+    grey: "#333333",
+    white: "#F2F2F2",
+  };
